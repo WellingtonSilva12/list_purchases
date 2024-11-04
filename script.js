72b22dbfc4a9d94e2b7a7575cac6c8f1ed58e77d
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2024-11-04 13:46:23 -0300

    creation of the function that adds items, adds and deletes

--- script.js
+++ script.js
@@ -14,6 +14,62 @@ const toggleAddItemForm = () => {
     blackBackdrop.classList.toggle('active');
 };
 
+const addItemToList = (event) => {
+    event.preventDefault(); 
+
+    const productName = document.getElementById('product-name').value;
+    const productValue = parseFloat(document.getElementById('product-value').value);
+    const productQuantity = parseInt(document.getElementById('product-quantity').value);
+
+    if (productName === "" || isNaN(productValue) || isNaN(productQuantity)) {
+        alert('Preencha todos os campos corretamente');
+        return;
+    }
+
+    const productSubtotal = productValue * productQuantity;
+
+    const productItem = document.createElement('li');
+    productItem.classList.add('product-item');
+
+    productItem.innerHTML = `
+        <input type="checkbox" class="product-checkbox">
+        <span class="product-name">${productName}</span> 
+        <span class="product-quantity">(${productQuantity}x)</span> 
+        <span class="product-subtotal">R$ ${productSubtotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>
+        <button class="remove-btn">Apagar</button>
+    `;
+
+    productsList.appendChild(productItem);
+
+    productForm.reset();
+    toggleAddItemForm();
+
+    const checkbox = productItem.querySelector('.product-checkbox');
+    const removeBtn = productItem.querySelector('.remove-btn');
+
+    checkbox.addEventListener('change', (e)=> {
+        if (e.target.checked) {
+            total += productSubtotal;
+        } else {
+            total -= productSubtotal;
+        }
+        totalValue.textContent = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    });
+
+    removeBtn.addEventListener('click', () => {
+        if (checkbox.checked) {
+            total -= productSubtotal
+            totalValue.textContent = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        }
+        removeItemFromList(productItem)
+    });
+};
+
+const removeItemFromList = (item) => {
+    item.remove();
+};
+
+
 addItemBtn.addEventListener('click', toggleAddItemForm);
 cancelBtn.addEventListener('click', toggleAddItemForm);
 blackBackdrop.addEventListener('click', toggleAddItemForm);
